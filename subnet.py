bce77fefc99fbb6860af733786c90e987a04cc44
Thanabodee <wingyminus@gmail.com>
2013-05-18 14:15:00 +0700

    move netmask to IPNetwork and refactoring code

--- subnet.py
+++ subnet.py
@@ -2,30 +2,12 @@ import re
 
 class MyIPAddress(object):
 
-    def __init__(self, ip, has_netmask=True):
-        self._ip, self._netmask = [self._tuple_from(addr) for addr in self._split(ip)]
-
-    @property
-    def addr(self):
-        return '.'.join([str(d) for d in self._ip])
+    def __init__(self, ip):
+        self._ip = self._tuple_from(ip)
 
     @property
     def words(self):
         return self._ip
-
-    @property
-    def netmask(self):
-        return '.'.join([str(d) for d in self._netmask])
-
-
-    def _split(self, ip):
-        if re.match(r'^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/\d{2}$', ip):
-            addr, netmask = ip.split('/')
-        else:
-            addr, netmask = ip, '255.255.255.255'
-
-        return addr, netmask
-
 
     def _tuple_from(self, ip):
         # matching pattern 11111111.11111111.11111111.11111111
@@ -34,11 +16,27 @@ class MyIPAddress(object):
         # matching pattern 255.255.255.255
         elif re.match(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$', ip):
             return tuple([int(d) for d in ip.split('.')])
-        # matching pattern 255.255.255.255/32
-        elif re.match(r'^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/\d{2}$', ip):
-            return tuple([int(d) for d in ip.split('/')[0].split('.')])
-        elif re.match(r'^\d{2}$', ip):
-            binary_addr = '1' * int(ip) + '0' * (32 - int(ip))
-            binary_addr_with_dot = re.sub(r'^([0-1]{8})([0-1]{8})([0-1]{8})([0-1]{8})$', r'\1.\2.\3.\4', binary_addr)
+        # matching pattern 32
+        elif re.match(r'^\d{1,2}', ip):
+            to_binary_str = lambda ip: '1' * int(ip) + '0' * (32 - int(ip))
+            binary_addr = re.sub(r'^(\d{8})(\d{8})(\d{8})(\d{8})$', r'\1.\2.\3.\4', to_binary_str(ip))
 
-            return self._tuple_from(binary_addr_with_dot)
+            return self._tuple_from(binary_addr)
+
+    def __str__(self):
+        return ".".join([str(word) for word in self._ip])
+
+class MyIPNetwork(object):
+
+    def __init__(self, ip_network):
+        # matching pattern 255.255.255.255/32
+        if re.match(r'^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/\d{1,2}', ip_network):
+            self._addr, self._netmask = [MyIPAddress(ip) for ip in ip_network.split('/')]
+
+    @property
+    def addr(self):
+        return self._addr
+
+    @property
+    def netmask(self):
+        return self._netmask
--- subnet_test.py
+++ subnet_test.py
@@ -1,38 +1,38 @@
 from nose.tools import eq_, ok_
-from subnet import MyIPAddress
+from subnet import MyIPAddress, MyIPNetwork
 
 def test_address_with_given_binary_address_1():
     my_ip = MyIPAddress('11111111.11111111.11111111.11111111')
-    eq_(my_ip.addr, '255.255.255.255')
+    eq_(str(my_ip), '255.255.255.255')
 
 def test_address_with_given_binary_address_2():
     my_ip = MyIPAddress('11111111.11111111.11111111.00000000')
-    eq_(my_ip.addr, '255.255.255.0')
+    eq_(str(my_ip), '255.255.255.0')
 
 def test_address_with_given_dec_address_1():
     my_ip = MyIPAddress('255.255.255.255')
-    eq_(my_ip.addr, '255.255.255.255')
+    eq_(str(my_ip), '255.255.255.255')
 
 def test_address_with_given_dec_address_2():
     my_ip = MyIPAddress('192.168.5.0')
-    eq_(my_ip.addr, '192.168.5.0')
-
-def test_address_with_given_ip_and_subnet_mask():
-    my_ip = MyIPAddress('192.168.5.0/26')
-    eq_(my_ip.addr, '192.168.5.0')
+    eq_(str(my_ip), '192.168.5.0')
 
 def test_word_must_return_tuple_1():
     my_ip = MyIPAddress('1.1.1.1')
     eq_(my_ip.words, (1, 1, 1, 1))
 
-def test_word_must_return_tuple_2():
-    my_ip = MyIPAddress('192.168.5.0/26')
-    eq_(my_ip.words, (192, 168, 5, 0))
-
 def test_word_must_return_tuple_3():
     my_ip = MyIPAddress('11111111.11111111.11111111.11111111')
     eq_(my_ip.words, (255, 255, 255, 255))
 
-def test_extract_subnet_from_constructor():
-    my_ip = MyIPAddress('192.168.5.0/26')
-    eq_(my_ip.netmask, '255.255.255.192')
+def test_addr_with_given_number_of_binary():
+    my_ip = MyIPAddress('26')
+    eq_(str(my_ip), '255.255.255.192')
+
+def test_ip_network():
+    ip = MyIPNetwork('192.168.5.0/26')
+
+    ok_(isinstance(ip.addr, MyIPAddress))
+    ok_(isinstance(ip.netmask, MyIPAddress))
+    eq_(str(ip.addr), '192.168.5.0')
+    eq_(str(ip.netmask), '255.255.255.192')
